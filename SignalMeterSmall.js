ba0c51f9b7c748970acbf3f0db70960db12b882b
AmateurAudioDude <168192910+AmateurAudioDude@users.noreply.github.com>
2024-06-29 07:55:26 +1000

    v1.2.0 - Squelch

--- SignalMeterSmall.js
+++ SignalMeterSmall.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'Signal Meter Small',
-    version: '1.1.5',
+    version: '1.2.0',
     author: 'AAD',
     frontEndPath: 'SignalMeterSmall/pluginSignalMeterSmall.js'
 }
