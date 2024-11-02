3f0473d6ad59ab984a7bbbdd90803be70822e22c
Amateur Audio Dude <168192910+AmateurAudioDude@users.noreply.github.com>
2024-11-02 17:15:09 +1100

    v1.3.3

--- SignalMeterSmall.js
+++ SignalMeterSmall.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'Signal Meter Small',
-    version: '1.3.2',
+    version: '1.3.3',
     author: 'AAD',
     frontEndPath: 'SignalMeterSmall/pluginSignalMeterSmall.js'
 }
